9f8105b33e18b5e9416d6b1c1c9f4b0fdac9f249
Luke Emmanuel .C. <124929220+Ranking-Topnotch@users.noreply.github.com>
2025-03-31 20:07:37 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(
       resave: false,
       saveUninitialized: false,
       cookie: {
-          secure: true
+          secure: false
       }, // Set secure to true if using HTTPS
         
     })
